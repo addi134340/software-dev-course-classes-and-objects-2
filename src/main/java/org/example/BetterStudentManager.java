8512521d01c3e93673a0ac6261e7ca2a896cd252
Addison Fowler <fowler.addison13@gmail.com>
2025-06-15 23:51:45 -0500

    Completed Assignment

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -12,10 +12,16 @@ public class BetterStudentManager {
 
     public void addStudent(String name, double grade) {
         // This method should add a new student to the list of students
+        Student student = new Student();
+        student.name = name;
+        student.grade = grade;
+
+        students.add(student);
     }
 
     public void removeStudent(String name) {
         // This method should remove the student with the given name from the list of students
+       students.removeIf(student -> student.name.equals(name));
     }
 
     public String getStudentList() {
@@ -29,6 +35,16 @@ public class BetterStudentManager {
         // looping through the list of students
         //
         // Replace the following line with your implementation
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        for (Student student : students) {
+            result.append(student.name)
+                    .append(" ")
+                    .append(student.grade)
+                    .append("\n");
+        }
+
+        return result.toString();
     }
 }
